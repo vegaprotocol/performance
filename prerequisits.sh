0e0ba90055d8e49abaf5b64843d38ed9d030bcf0
Peter Barrow <pete@vega.xyz>
2023-05-02 15:02:42 +0100

    feat: Updates

--- prerequisits.sh
+++ prerequisits.sh
@@ -3,7 +3,7 @@
 ## Check PERFHOME is set or we have to stop
 if [ ! -v PERFHOME ]
 then
-  echo Please set the $PERFHOME variable to the root of the performance repo
+  echo Please set the PERFHOME environment variable to the root of the performance repo
   exit
 fi
 
@@ -30,15 +30,21 @@ fi
 if ! command -v go &> /dev/null
 then
   echo "Please install the latest version of golang"
+  echo "$ wget https://go.dev/dl/go1.19.8.linux-amd64.tar.gz"
+  echo "$ sudo rm -rf /usr/local/go"
+  echo "$ sudo tar -C /usr/local -xzf go1.19.8.linux-amd64.tar.gz" 
+  echo "Then add /usr/local/go/bin to your PATH variable"
   exit
 else
   echo "Golang installed correctly"
 fi
+export CGO_ENABLED=0
 
 ## git
 if ! command -v git &>/dev/null
 then
   echo "Please install the latest version of git"
+  echo "sudo apt install git -y"
   exit
 else
   echo "Git installed correctly"
@@ -48,6 +54,7 @@ fi
 if ! command -v sqlite &>/dev/null
 then
   echo "Please install the latest version of SQLite"
+  echo "sudo apt install sqlite -y"
   exit
 else
   echo "SQLite install correctly"
@@ -57,6 +64,7 @@ fi
 if ! command -v datamash &>/dev/null
 then
   echo "Please install the latest version of datamash"
+  echo "sudo apt install datamash -y"
   exit
 else
   echo "Datamash install correctly"
@@ -66,6 +74,11 @@ fi
 if ! command -v docker &>/dev/null
 then
   echo "Please install the latest version of docker"
+  echo "sudo apt install docker.io -y"
+  echo "sudo groupadd docker"
+  echo "sudo usermod -aG docker \$USER"
+  echo "newgrp docker"
+
   exit
 else
   echo "Docker install correctly"
